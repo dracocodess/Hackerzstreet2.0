5455fbffddd3c4b19dd73bfb9681fe1038b473e6
Abhinav Mondal <134502360+abhinav7889@users.noreply.github.com>
2024-02-16 19:57:54 +0530

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -9,6 +9,8 @@ export default function Hero() {
 
         <Image src="/assets/Asset 7hackerstreet mobiile element new.svg" alt='Hackerstreet' width={1920} height={1080} className='w-full h-full md:w-1/2' />
         <div className='block text-wrap break-words px-4 py-12 mt-5'>
+        <Image src="/assets/ieee-logo-white.svg" alt="logo" className="w-64 md:w-72 mb-10" width={1960} height={1080}/>
+
             <span className='text-[#d6d5e3] lg:text-[4rem] md:text-3xl text-3xl font-bold poppins'>{t("HACKERZSTREET")}</span>
             <span className='text-brand-green lg:text-[4rem] md:text-3xl poppins font-bold text-3xl'>{" "} {t('2')}</span>
 
