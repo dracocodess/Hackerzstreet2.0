322bc78603ed376597ed651fd464332fe4545728
abhinav7889 <abhinav200503@gmail.com>
2024-04-03 20:15:25 +0530

    fix

--- components/sections/Track.js
+++ components/sections/Track.js
@@ -12,16 +12,16 @@ export default function Tracks() {
         whileInView="whileInView"
         initial="initial"
         className="relative rounded-[33px] w-full items-center block overflow-hidden md:pb-12 md:px-12 px-4 pb-4 bg-gradient-31
-        from-orange-400 to-orange-300 py-10 -z-10">
+        from-lime-500 to-lime-400 py-10 -z-10">
         <div className="overflow-hidden w-fit mx-auto relative rounded-md m-10 px-2">
             <div id="header-section-tracks"
                  className="-left-2 w-0 bg-blue-300 rounded-md h-full absolute top-0 -z-10"></div>
-            <h1 className="text-6xl font-bold poppins text-center mx-auto bg-gradient-to-r ease-in-out via-lime-300 bg-clip-text text-transparent
-            from-green-400 to-green-300
+            <h1 className="text-6xl font-bold poppins text-center mx-auto bg-gradient-to-r ease-in-out via-red-400 bg-clip-text text-transparent
+            from-orange-400 to-orange-500
             animate-text z-10">{t('Tracks')}</h1>
         </div>
         <div className="h-2"/>
-        <h2 className="text-center mx-auto font-bold text-xl poppins text-lime-300">{t('Our tracks')}</h2>
+        <h2 className="text-center mx-auto font-bold text-xl poppins text-orange-400">{t('Our tracks')}</h2>
         <div className="h-10"/>
         {TRACK_META.map(({name: track, description: desc}, index) => (
             <TrackCard key={index} pi={++index} track={track} desc={desc}/>
