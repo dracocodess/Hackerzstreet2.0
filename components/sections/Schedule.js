61e669d6a040d33a16828e1e7bc68c78ad19e984
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2024-04-05 14:09:43 +0530

    ok

--- components/sections/Schedule.js
+++ components/sections/Schedule.js
@@ -1,7 +1,7 @@
-import ScheduleCard from "../ScheduleCard";
+import ScheduleCard from "./ScheduleCard";
 import {motion, useScroll} from "framer-motion"
 import {useTranslation} from "react-i18next";
-import ANIM_STATE_SECTION from "../../anim/section";
+import ANIM_STATE_SECTION from "../anim/section";
 
 export default function Schedule() {
     const {t} = useTranslation();
@@ -27,11 +27,9 @@ export default function Schedule() {
                         {t('Hackathon Begins')}
                     </div>
                     <div className="h-5"/>
-                    <ScheduleCard day={"Day 1"} time={"06:00 PM"}
-                                  event={"Opening Ceremony & Problem Statements release"}/>
+                    <ScheduleCard day={"Day 1"} time={"06:00 PM"} event={"Opening Ceremony & Problem Statements release"}/>
                     <div className="border-dashed border-l-2 border-purple-400 w-1 h-16 absolute left-1/2"></div>
-                    <div
-                        className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
+                    <div className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
                         <motion.div className="w-full h-1 rounded-full bg-purple-500" initial={{
                             height: "0%",
                             y: "0%"
@@ -46,12 +44,11 @@ export default function Schedule() {
                             y: ["0%", "100%"]
                         }}/>
                     </div>
-
-
-                    <ScheduleCard day={"Day 2"} time={"03:00 AM"} event={"Round 1 submissions"}/>
+                   
+                    
+                    <ScheduleCard day={"Day 2"} time={"04:00 AM"} event={"Round 1 submissions"}/>
                     <div className="border-dashed border-l-2 border-purple-400 w-1 h-16 absolute left-1/2"></div>
-                    <div
-                        className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
+                    <div className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
                         <motion.div className="w-full h-1 rounded-full bg-purple-500" initial={{
                             height: "0%",
                             y: "0%"
@@ -68,8 +65,7 @@ export default function Schedule() {
                     </div>
                     <ScheduleCard day={"Day 2"} time={"06:00 AM"} event={"Round 1 results"}/>
                     <div className="border-dashed border-l-2 border-purple-400 w-1 h-16 absolute left-1/2"></div>
-                    <div
-                        className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
+                    <div className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
                         <motion.div className="w-full h-1 rounded-full bg-purple-500" initial={{
                             height: "0%",
                             y: "0%"
@@ -84,10 +80,9 @@ export default function Schedule() {
                             y: ["0%", "100%"]
                         }}/>
                     </div>
-                    <ScheduleCard day={"Day 2"} time={"10:00 AM"} event={"Reporting and final mentoring session"}/>
+                    <ScheduleCard day={"Day 2"} time={"01:30 PM"} event={"Round 1 qualified teams need to reach Smt. Sharada Pai Auditorium"}/>
                     <div className="border-dashed border-l-2 border-purple-400 w-1 h-16 absolute left-1/2"></div>
-                    <div
-                        className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
+                    <div className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
                         <motion.div className="w-full h-1 rounded-full bg-purple-500" initial={{
                             height: "0%",
                             y: "0%"
@@ -102,25 +97,7 @@ export default function Schedule() {
                             y: ["0%", "100%"]
                         }}/>
                     </div>
-                    <ScheduleCard day={"Day 2"} time={"02:00 PM"} event={"Round 2 Presentation starts"}/>
-                    <div className="border-dashed border-l-2 border-purple-400 w-1 h-16 absolute left-1/2"></div>
-                    <div
-                        className="w-2 h-16 bg-transparent relative rounded-full overflow-hidden mx-auto left-[0.1rem]">
-                        <motion.div className="w-full h-1 rounded-full bg-purple-500" initial={{
-                            height: "0%",
-                            y: "0%"
-                        }} animate={{
-                            transition: {
-                                repeat: Infinity,
-                                duration: 3,
-                                type: "tween",
-                                ease: "easeInOut"
-                            },
-                            height: ["0%", '100%'],
-                            y: ["0%", "100%"]
-                        }}/>
-                    </div>
-                    <ScheduleCard day={"Day 2"} time={"06:00 PM"} event={"Results and closing ceremony"}/>
+                    <ScheduleCard day={"Day 2"} time={"02:00 PM"} event={"Judging & prize distribution"}/>
                 </ol>
 
 
